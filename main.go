14fcf0f171ba1b82bd6fc000dc9717da7f80a228
raymondragon <54016243+raymondragon@users.noreply.github.com>
2024-02-13 15:58:35 +0800

    更新 main.go

--- main.go
+++ main.go
@@ -6,19 +6,27 @@ import (
     "os"
     "strings"
     "github.com/google/uuid"
+    "golang.org/x/net/webdav"
 )
 var (
     auth = flag.String("a", "/auth", "")
     bind = flag.String("b", ":8080", "")
+    cust = flag.String("c", "/webd", "")
+    dirt = flag.String("d", ".", "")
 )
 func main() {
     flag.Parse()
     http.HandleFunc(*auth, func(w http.ResponseWriter, r *http.Request) {
         ip := strings.Split(r.RemoteAddr, ":")[0]
         w.Write([]byte("ID: " + uuid.New().String() + "\n" + "IP: " + ip + "\n"))
-        file, _ := os.OpenFile("ip.list", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+        file, _ := os.OpenFile("IPlist", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
         defer file.Close()
         file.WriteString(ip + "\n")
+    })
+    http.Handle(*cust + "/", &webdav.Handler{
+        FileSystem: webdav.Dir(*dirt),
+        Prefix:     *cust,
+        LockSystem: webdav.NewMemLS(),
     })
     log.Fatal(http.ListenAndServe(*bind, nil))
 }
