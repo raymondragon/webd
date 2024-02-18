a2ced9729f8ebefed90659aab228c91c87d7998d
raymondragon <54016243+raymondragon@users.noreply.github.com>
2024-02-18 15:56:45 +0800

    Update and rename main.go to main_test.go

--- main_test.go
+++ main_test.go
@@ -5,13 +5,10 @@ import (
     "net"
     "net/http"
     "os"
-    "golang.org/x/net/webdav"
 )
 var (
     auth = flag.String("a", "/auth", "")
     bind = flag.String("b", ":8080", "")
-    cust = flag.String("c", "/webd", "")
-    dirt = flag.String("d", ".", "")
 )
 func main() {
     flag.Parse()
@@ -21,11 +18,6 @@ func main() {
         file, _ := os.OpenFile("IPlist", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
         defer file.Close()
         file.WriteString(ip+"\n")
-    })
-    http.Handle(*cust+"/", &webdav.Handler{
-        FileSystem: webdav.Dir(*dirt),
-        Prefix:     *cust,
-        LockSystem: webdav.NewMemLS(),
     })
     log.Fatal(http.ListenAndServe(*bind, nil))
 }
