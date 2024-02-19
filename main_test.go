7301173d00229e9ea95e5ccc3d51eb94a88ba14b
raymondragon <54016243+raymondragon@users.noreply.github.com>
2024-02-19 09:50:00 +0800

    Update main_test.go

--- main_test.go
+++ main_test.go
@@ -5,19 +5,28 @@ import (
     "net"
     "net/http"
     "os"
+    "sync"
 )
 var (
     auth = flag.String("a", "/auth", "")
     bind = flag.String("b", ":8080", "")
+    mute = sync.Mutex
 )
 func main() {
     flag.Parse()
+    file, err := os.OpenFile("IPlist", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+    if err != nil {
+        log.Fatal("[ERR-0] ", err)
+    }
+    defer file.Close()
     http.HandleFunc(*auth, func(w http.ResponseWriter, r *http.Request) {
         ip, _, _ := net.SplitHostPort(r.RemoteAddr)
         w.Write([]byte(ip+"\n"))
-        file, _ := os.OpenFile("IPlist", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
-        defer file.Close()
-        file.WriteString(ip+"\n")
+        mute.Lock()
+        defer mute.Unlock()
+        if _, err := file.WriteString(ip + "\n"); err != nil {
+            log.Println("[ERR-1] ", err)
+        }
     })
     log.Fatal(http.ListenAndServe(*bind, nil))
 }
