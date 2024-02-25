9aa44a51ae755d219bc29972b75ea40d938ca252
raymondragon <54016243+raymondragon@users.noreply.github.com>
2024-02-25 13:47:01 +0800

    Update tls.go

---
+++ tls.go
@@ -0,0 +1,77 @@
+package main
+import (
+    "crypto/rand"
+    "crypto/rsa"
+    "crypto/tls"
+    "crypto/x509"
+    "crypto/x509/pkix"
+    "encoding/pem"
+    "flag"
+    "log"
+    "math/big"
+    "net/http"
+    "time"
+    "golang.org/x/net/webdav"
+)
+var (
+    addr = flag.String("a", ":1", "addr")
+    dirt = flag.String("d", "./", "dirt")
+    path = flag.String("p", "/x", "path")
+)
+func main() {
+    flag.Parse()
+    webd := &webdav.Handler{
+        FileSystem: webdav.Dir(*dirt),
+        Prefix:     *path,
+        LockSystem: webdav.NewMemLS(),
+    }
+    cert, err := generateCert()
+    if err != nil {
+        log.Fatalf("[ERR-0] %v", err)
+    }
+    tlsConfig := &tls.Config{
+        Certificates: []tls.Certificate{cert},
+    }
+    serv := &http.Server{
+        Addr:      *addr,
+        Handler:   webd,
+        TLSConfig: tlsConfig,
+    }
+    log.Printf("[LISTEN] %v%v [SERVE] %v", *addr, *path, *dirt)
+    if err := serv.ListenAndServeTLS("", ""); err != nil {
+        log.Fatalf("[ERR-1] %v", err)
+    }
+}
+
+
+func generateCert() (tls.Certificate, error) {
+    priv, err := rsa.GenerateKey(rand.Reader, 2048)
+    if err != nil {
+        return tls.Certificate{}, err
+    }
+    serialNumber, err := rand.Int(rand.Reader, new(big.Int).Lsh(big.NewInt(1), 128))
+    if err != nil {
+        return tls.Certificate{}, err
+    }
+    template := x509.Certificate{
+        SerialNumber: serialNumber,
+        Subject: pkix.Name{
+            Organization: []string{"Self-signed"},
+        },
+        NotBefore:             time.Now(),
+        NotAfter:              time.Now().Add(365*10*24*time.Hour),
+        KeyUsage:              x509.KeyUsageKeyEncipherment | x509.KeyUsageDigitalSignature,
+        ExtKeyUsage:           []x509.ExtKeyUsage{x509.ExtKeyUsageServerAuth},
+        BasicConstraintsValid: true,
+    }
+    certDER, err := x509.CreateCertificate(rand.Reader, &template, &template, &priv.PublicKey, priv)
+    if err != nil {
+        return tls.Certificate{}, err
+    }
+
+    certPEM := pem.EncodeToMemory(&pem.Block{Type: "CERTIFICATE", Bytes: certDER})
+    keyDER := x509.MarshalPKCS1PrivateKey(priv)
+    keyPEM := pem.EncodeToMemory(&pem.Block{Type: "RSA PRIVATE KEY", Bytes: keyDER})
+
+    return tls.X509KeyPair(certPEM, keyPEM)
+}
