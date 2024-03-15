f8b1bb94338b69b7b6244ba98e49bf2b343b1095
raymondragon <54016243+raymondragon@users.noreply.github.com>
2024-03-15 10:14:46 +0800

    Update custom.go

---
+++ custom.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+    "flag"
+    "log"
+
+    "github.com/raymondragon/golib"
+)
+
+var rawURL = flag.String("url", "", "http://host:port/path#dir")
+
+func main() {
+    flag.Parse()
+    parsedURL, err := golib.URLParse(*rawURL)
+    if err != nil {
+        log.Printf("[WARN] %v", err)
+    }
+    webdavHandler := golib.WebdavHandler(parsedURL.Fragment, parsedURL.Path)
+    switch parsedURL.Scheme {
+    case "http":
+        log.Printf("[INFO] %v", *rawURL)
+        if err := golib.ServeHTTP(parsedURL.Hostname, parsedURL.Port, webdavHandler); err != nil {
+            log.Fatalf("[ERRO] %v", err)
+        }
+    default:
+        log.Fatalf("[ERRO] %v", parsedURL.Scheme)
+    }
+}
