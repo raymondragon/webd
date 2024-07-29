e5fb4b4d2e4933fac4a80cc89ed4a5bf2c71fa28
raymondragon <54016243+raymondragon@users.noreply.github.com>
2024-07-29 11:28:47 +0800

    Update webd.go

--- webd.go
+++ webd.go
@@ -1,27 +1,24 @@
 package main
 
 import (
-    "flag"
     "log"
     "net/url"
+    "os"
 
     "github.com/raymondragon/golib"
 )
 
-var rawURL = flag.String("u", "", "http://name:port/path#dir")
-
 func main() {
-    flag.Parse()
-    if *rawURL == "" {
-        flag.Usage()
-        log.Fatalf("[ERRO] %v", "Invalid Flag")
+    if len(os.Args) < 2 {
+        log.Fatalf("[ERRO] Usage: http://name:port/path#dir")
     }
-    parsedURL, err := url.Parse(*rawURL)
+    rawURL := os.Args[1]
+    parsedURL, err := url.Parse(rawURL)
     if err != nil {
         log.Printf("[WARN] %v", err)
     }
     webdavHandler := golib.WebdavHandler(parsedURL.Fragment, parsedURL.Path)
-    log.Printf("[INFO] %v", *rawURL)
+    log.Printf("[INFO] %v", rawURL)
     if err := golib.ServeHTTP(parsedURL.Host, webdavHandler); err != nil {
         log.Fatalf("[ERRO] %v", err)
     }
